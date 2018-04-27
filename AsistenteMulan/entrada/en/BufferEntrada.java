57005a9ca4fb02ef63a6734e65a97d305fd4f3ab
LucianoArtale <lartale@circostudio.com>
2018-04-27 19:16:11 -0300

    Inicializacion

---
+++ AsistenteMulan/entrada/en/BufferEntrada.java
@@ -0,0 +1,27 @@
+package en;
+
+import java.util.Scanner;
+
+public class BufferEntrada {
+	
+	private String entrada;
+	
+	private static String leerBuffer() {
+		Scanner sc = new Scanner(System.in);
+		return sc.nextLine();
+	}
+	
+	public BufferEntrada() {
+		this.entrada = BufferEntrada.leerBuffer();	
+	}
+
+	public String getEntrada() {
+		return entrada;
+	}
+	
+	
+	
+	
+	
+
+}
